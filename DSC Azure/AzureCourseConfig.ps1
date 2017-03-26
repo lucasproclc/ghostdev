84b8cf59b9c410bc039c57506eeb8eeb43588160
lucasproclc <eric.lucas@centurylink.com>
2017-03-26 03:44:43 -0700

    DSC Resource and Azure Demo

---
+++ DSC Azure/AzureCourseConfig.ps1
@@ -0,0 +1,33 @@
+configuration AzureCourseConfig
+{
+	Node WebServer
+	{
+		WindowsFeature IIS
+		{
+			Ensure					='Present'
+			Name					='Web-Server'
+			IncludeAllSubFeature 	=$true
+		}	
+		Group Developers
+		{	
+			Ensure					='Present'
+			GroupName				='DevGroup'
+		}
+		Accountant Group
+		{
+			Ensure					='Absent'
+			GroupName				='AcctGroup'
+		}
+		File DirectoryCreate
+		{
+			Ensure	="Present"
+			Type	="Directory"
+			DestinationPath	="C:\Users\Public\Document\MyDemo
+		}
+		Log AfterDirectoryCreate
+		{
+			Message					='Directory create using DSC'
+			DependsOn				='[File]DirectoryCreate'
+		}
+	}	
+}	
